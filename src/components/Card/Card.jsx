0216496a6975a5778ed034d4afe1563d158801a6
David Levente Dozsa <dozsalevi@gmail.com>
2020-01-21 20:02:35 +0000

    + Card with react spring

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+import './styles.css'
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+const Card = ( {image} ) => {
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+  return (
+    <animated.div
+      class="card"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans) }}
+    >
+      <img src={image} alt="" />
+    </animated.div>
+  )
+}
+
+export default Card
